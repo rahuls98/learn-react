9c78abae8269883897dc138bd6f11f91741647ae
Rahul Suresh <rahs98@gmail.com>
2024-02-16 17:00:02 -0500

    Class work

--- src/pages/components/list_plain.js
+++ src/pages/components/list_plain.js
@@ -1,14 +1,12 @@
 const people = [
-  'Creola Katherine Johnson: mathematician',
-  'Mario José Molina-Pasquel Henríquez: chemist',
-  'Mohammad Abdus Salam: physicist',
-  'Percy Lavon Julian: chemist',
-  'Subrahmanyan Chandrasekhar: astrophysicist'
+    "Creola Katherine Johnson: mathematician",
+    "Mario José Molina-Pasquel Henríquez: chemist",
+    "Mohammad Abdus Salam: physicist",
+    "Percy Lavon Julian: chemist",
+    "Subrahmanyan Chandrasekhar: astrophysicist",
 ];
 
 export default function List() {
-  const listItems = people.map(person =>
-    <li>{person}</li>
-  );
-  return <ul>{listItems}</ul>;
+    const listItems = people.map((person, ind) => <li key={ind}>{person}</li>);
+    return <ul>{listItems}</ul>;
 }
--- src/pages/qcomps/recipes.js
+++ src/pages/qcomps/recipes.js
@@ -1,22 +1,60 @@
-export const recipes = [{
-  id: 'greek-salad',
-  name: 'Greek Salad',
-  ingredients: new Set(['tomatoes', 'cucumber', 'onion', 'olives', 'feta'])
-}, {
-  id: 'hawaiian-pizza',
-  name: 'Hawaiian Pizza',
-  ingredients: new Set(['pizza crust', 'pizza sauce', 'mozzarella', 'ham', 'pineapple'])
-}, {
-  id: 'hummus',
-  name: 'Hummus',
-  ingredients: new Set(['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'])
-}];
+export const recipes = [
+    {
+        id: "greek-salad",
+        name: "Greek Salad",
+        ingredients: new Set([
+            "tomatoes",
+            "cucumber",
+            "onion",
+            "olives",
+            "feta",
+        ]),
+    },
+    {
+        id: "hawaiian-pizza",
+        name: "Hawaiian Pizza",
+        ingredients: new Set([
+            "pizza crust",
+            "pizza sauce",
+            "mozzarella",
+            "ham",
+            "pineapple",
+        ]),
+    },
+    {
+        id: "hummus",
+        name: "Hummus",
+        ingredients: new Set([
+            "chickpeas",
+            "olive oil",
+            "garlic cloves",
+            "lemon",
+            "tahini",
+        ]),
+    },
+];
+
+const RecipeItem = ({ recipe }) => {
+    let ingredients = Array.from(recipe.ingredients) || [];
+    return (
+        <div>
+            <h1>{recipe.name}</h1>
+            <ul>
+                {ingredients.map((ingredient) => (
+                    <li>{ingredient}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default function RecipeList() {
-  return (
-    <div>
-      <h1>Recipes</h1>
-      {}
-    </div>
-  );
+    return (
+        <div>
+            <h1>Recipes</h1>
+            {recipes.map((recipe) => (
+                <RecipeItem recipe={recipe} />
+            ))}
+        </div>
+    );
 }
